f23df977a41b13811deab38cf5b3383e94d007e5
tybca45 <tybca45@dca.com>
2022-07-23 03:03:26 +0530

    sec commit

---
+++ Assignments/Lab-4/Answers/task3.js
@@ -0,0 +1,6 @@
+// Task -3
+// Install a package “express” locally in your system
+// Install a package “nodemon” and “yarn”globally in your system
+// Uninstall “express” package.
+// Install a package “cors” with yarn
+// Search about the module “express”
---
+++ Assignments/Lab-4/Answers/task4.js
@@ -0,0 +1,4 @@
+//Print the Directory name
+console.log(__dirname); 
+//Print the File name
+console.log(__filename); 
